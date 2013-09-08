68a66f9b26beb8b7d44dbc7275e68527f822ed08
Alvaro Hilario <king314@gmail.com>
2013-09-08 09:58:51 -0700

    Update array_codinova.php

--- array_codinova.php
+++ array_codinova.php
@@ -1,20 +1,18 @@
 <html>
     <p>
 	<?php
-	// Create an array and push on the names
-    // of your closest family and friends
-    $codinova = array();
-    array_push($codinova, "Welling");
-    array_push($codinova, "Ariel");
-    array_push($codinova, "Anthuan");
-    array_push($codinova, "Waldo");
-    array_push($codinova, "Alvaro");
-	// Sort the list
-    sort($codinova);
-	// Randomly select a winner!
-    $winner = strtoupper($codinova[rand(0, count($codinova))]);
-	// Print the winner's name in ALL CAPS
-	echo $winner;
+	    $codinova = array();
+	    array_push($codinova, "Welling");
+	    array_push($codinova, "Ariel");
+	    array_push($codinova, "Anthuan");
+	    array_push($codinova, "Waldo");
+	    array_push($codinova, "Alvaro");
+	
+	    sort($codinova);
+		
+	    $winner = strtoupper($codinova[rand(0, count($codinova))]);
+	
+	    echo $winner;
 	?>
 	</p>
 </html>
